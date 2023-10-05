616486d17c2ad6950794c07bebd85cad2da87435
ALPER TUNGA <alper@altu.dev>
2023-10-05 21:07:52 +0300

    [Feature]: Password Generator class has been created successfully.
    * In this lecture we've created a password generator class to implement different algorithm techniques.

--- YetGenAkbankJump.OOPConsole/Program.cs
+++ YetGenAkbankJump.OOPConsole/Program.cs
@@ -1,147 +1,33 @@
-﻿using System.Text.Json;
-using Microsoft.Extensions.Logging;
-using YetGenAkbankJump.OOPConsole.Common;
-using YetGenAkbankJump.OOPConsole.Entities;
-using YetGenAkbankJump.OOPConsole.Services;
+﻿
+using YetGenAkbankJump.OOPConsole.Utilities;
 
-ILogger logger = new Logger<string>(new LoggerFactory());
+var passwordGenerator = new PasswordGenerator();
 
-const string logFilePath = "C:\\Users\\alper\\Desktop\\logs.txt";
+Console.WriteLine("Lütfen şifreniz için istediğiniz karakter sayısını giriniz.");
 
-var consoleLogger = new ConsoleLogger("Alper Hoca's Logger");
+var passwordLength = Convert.ToInt32(Console.ReadLine());
 
-var fileLogger = new FileLogger(logFilePath);
+Console.WriteLine("Şifreniz sayıları içersin mi?");
 
-List<AccessControlLog> logs = new();
+var includeNumbers = Console.ReadLine() == "evet";
 
-try
-{
-   
+Console.WriteLine("Şifreniz küçük karakterleri içersin mi?");
 
-    var filePath = "C:\\Users\\alper\\Desktop\\AccessControlLogs.txt";
+var includeLowerCase = Console.ReadLine() == "evet";
 
-    var textFile = File.ReadAllText(filePath);
+Console.WriteLine("Şifreniz büyük karakterleri içersin mi?");
 
-    consoleLogger.LogInfo("Text file imported.");
+var includeUpperCase = Console.ReadLine() == "evet";
 
-    fileLogger.LogInfo("Text file imported.");
+Console.WriteLine("Şifreniz özel karakterleri içersin mi?");
 
-    var splittedLines = textFile.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+var includeSpecialChars = Console.ReadLine() == "evet";
 
-    // 7---FRNM3121049B---FP---2023-08-09T10:15:32+00:00
+var password = passwordGenerator.Generate(passwordLength,includeNumbers,includeLowerCase,includeUpperCase,includeSpecialChars);
 
-    consoleLogger.LogInfo("Lines are splitted.");
-    fileLogger.LogInfo("Lines are splitted.");
+Console.WriteLine($"Şifreniz:{password}");
 
-   
+Console.ReadLine();
 
-    foreach (var splittedLine in splittedLines)
-    {
-        var values = splittedLine.Split("---", StringSplitOptions.RemoveEmptyEntries);
-
-        var accessControlLog = new AccessControlLog()
-        {
-            Id = Guid.NewGuid(),
-            CreatedOn = DateTimeOffset.Now,
-            PersonId = Convert.ToInt64(values[0]),
-            DeviceSerialNo = values[1],
-            AccessType = AccessControlLog.ConvertStringToAccessType(values[2]),
-            LogTime = Convert.ToDateTime(values[3])
-        };
-
-        logs.Add(accessControlLog);
-    }
-
-    consoleLogger.LogInfo(" All lines are converted to objects.");
-
-    fileLogger.LogInfo("All lines are converted to objects.");
-
-
-    File.WriteAllText("C:\\Users\\alper\\Desktop\\AccessControlLogsJson.txt", JsonSerializer.Serialize(logs));
-
-    consoleLogger.LogSuccess("The JSON file was successfully created.");
-
-    fileLogger.LogSuccess("The JSON file was successfully created.");
-
-    consoleLogger.LogSuccess("The operation was successfully completed.");
-
-    fileLogger.LogSuccess("The operation was successfully completed.");
-
-
-    Console.WriteLine(consoleLogger.Name);
-
-    consoleLogger.LogFatal(consoleLogger.Name);
-
-    Console.ReadLine();
-}
-catch (Exception ex)
-{
-    consoleLogger.LogError(ex.Message);
-
-    fileLogger.LogError(ex.Message);
-}
-
-
-
-List<object> myObjects = new List<object>();
-
-myObjects.Add(consoleLogger);
-
-myObjects.Add(fileLogger);
-
-logs.ForEach(x=>myObjects.Add(x));
-
-foreach (var myObject in myObjects)
-{
-    if (myObject is LoggerBase)
-    {
-        var baseLogger = myObject as LoggerBase;
-
-        baseLogger.LogInfo("Ben bir base logger'ım");
-    }
-
-
-    if (myObject is ICreatedByEntity)
-    {
-        var entity = myObject as ICreatedByEntity;
-
-        entity.CreatedOn = DateTimeOffset.Now;
-    }
-
-    if (myObject is IDeletedByEntity)
-    {
-        var entity = myObject as IDeletedByEntity;
-
-        entity.DeletedOn = DateTimeOffset.Now;
-        entity.IsDeleted = true;
-    }
-}
-
-decimal finalAmount=0;
-
-int amount = 68;
-
-double doubleAmount = 68.5d;
-
-finalAmount = amount;
-
-finalAmount = (decimal)doubleAmount;
-
-Teacher teacher = new Teacher()
-{
-    FirstName = "Alper",
-    LastName = "Tunga",
-    CreatedOn = DateTimeOffset.Now
-};
-
-Student student = new Student()
-{
-    FirstName = "Ahmet",
-    LastName = "Kök",
-    CreatedOn = DateTimeOffset.Now
-};
-
-
-teacher = student;
-
+return 1;
 
